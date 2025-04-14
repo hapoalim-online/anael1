e8ad4bd4a1b0ca1f44a90a9c327d136578a4149c
hapoalim-online <tzahi955@mail.com>
2025-04-14 10:44:05 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
 
 const CACHE_NAME = 'anael1-cache-v1';
 const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   '/css/all.min.css',
-  '/sounds/CARD.ogg',
-  '/sounds/no.ogg',
-  '/sounds/slot.ogg',
-  '/sounds/slot2.ogg',
-  '/sounds/yes.ogg',
-  '/webfonts/fa-brands-400.ttf',
-  '/webfonts/fa-brands-400.woff2',
-  '/webfonts/fa-regular-400.ttf',
-  '/webfonts/fa-regular-400.woff2',
-  '/webfonts/fa-solid-900.ttf',
-  '/webfonts/fa-solid-900.woff2',
-  '/webfonts/fa-v4compatibility.ttf',
-  '/webfonts/fa-v4compatibility.woff2'
+  './sounds/CARD.ogg',
+  './sounds/no.ogg',
+  './sounds/slot.ogg',
+  './sounds/slot2.ogg',
+  './sounds/yes.ogg',
+  './webfonts/fa-brands-400.ttf',
+  './webfonts/fa-brands-400.woff2',
+  './webfonts/fa-regular-400.ttf',
+  './webfonts/fa-regular-400.woff2',
+  './webfonts/fa-solid-900.ttf',
+  './webfonts/fa-solid-900.woff2',
+  './webfonts/fa-v4compatibility.ttf',
+  './webfonts/fa-v4compatibility.woff2'
 ];
 
 self.addEventListener('install', event => {
